63639dedf096c0f16cb0a557ef3723705780c71c
Ameejac <ameejacherukuru123@gmail.com>
2021-11-11 14:02:33 +0530

    ameeja[added:  starts with M capital letter code ]

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        string pattern = "^[M-Z]{3}[a-z]{3,}$";
+        static void Main(string[] args)
+        {
+            Regexpattern obj = new Regexpattern();
+            obj.validating();
+            
+        }
+
+    }
+}
---
+++ LogicalPrograms/Regex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LogicalPrograms
+{
+    class Regexpattern
+        
+    {
+        string pattern = "^[M-Z]{1,2}[a-z]{5,15}$";
+        string[] inputs= { "ameeja", "zzzz","MNghgjs", "zfhg","Rani","Shallu" ,"agfhf","Zghfgghf" };
+        public void validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("verifying the first letter start with capital M ");
+            iterationloop(inputs,regex);
+        }
+
+        public static void iterationloop(string[] array ,Regex regex)
+        {
+            
+                for (int i = 0; i < array.Length; i++)
+                {
+                    bool result = regex.IsMatch(array[i]);
+                    if (result)
+                    {
+                        Console.WriteLine(array[i] + " It is --->" + "Valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine(array[i] + " It is --->" + "Invalid");
+                    }
+                }
+        }
+
+        
+    }
+}
